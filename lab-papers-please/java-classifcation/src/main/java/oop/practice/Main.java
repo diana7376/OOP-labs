c9372ff6474b816d1cb80e5b2cde8d2980767d41
Diana7376 <dianelup3105@gmail.com>
2024-10-20 00:52:49 +0300

    fixed bug for classification (priority added)

--- lab-papers-please/java-classifcation/src/main/java/oop/practice/Main.java
+++ lab-papers-please/java-classifcation/src/main/java/oop/practice/Main.java
@@ -10,6 +10,52 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+  public static void assignToUniverse(Creature creature, String universeName, Universe starWars, Universe marvel, Universe hitchhikers, Universe rings) {
+    switch (universeName) {
+      case "Star Wars":
+        starWars.getIndividuals().add(creature);
+        break;
+      case "Marvel":
+        marvel.getIndividuals().add(creature);
+        break;
+      case "Hitchhiker's":
+        hitchhikers.getIndividuals().add(creature);
+        break;
+      case "Lord of the Rings":
+        rings.getIndividuals().add(creature);
+        break;
+    }
+  }
+
+  public static void applyTieBreaker(Creature creature, List<String> topUniverses, Universe starWars, Universe marvel, Universe hitchhikers, Universe rings) {
+    // Priority rule: humanoid and bulky creatures go to Lord of the Rings
+    if (creature.isHumanoid() != null && creature.isHumanoid() && creature.getTraits() != null && creature.getTraits().contains("BULKY")) {
+      if (topUniverses.contains("Lord of the Rings")) {
+        rings.getIndividuals().add(creature);
+        return;
+      }
+    }
+
+
+    if (creature.getTraits() != null && creature.getTraits().contains("GREEN") && creature.getTraits().contains("BULKY")) {
+      if (topUniverses.contains("Hitchhiker's")) {
+        hitchhikers.getIndividuals().add(creature);
+        return;
+      }
+    }
+
+   if (creature.isHumanoid() != null && creature.getTraits() != null && creature.isHumanoid().equals("false") && creature.getTraits().contains("BULKY")) {
+     if (topUniverses.contains("Hitchhiker's")){
+       hitchhikers.getIndividuals().add(creature);
+       return;
+     }
+   }
+
+
+    assignToUniverse(creature, topUniverses.get(0), starWars, marvel, hitchhikers, rings);
+  }
+
   public static void classifyCreature(Creature creature, Universe starWars, Universe marvel, Universe hitchhikers, Universe rings){
 
     //initial score
@@ -34,23 +80,23 @@ public class Main {
     if (planet !=null){
     switch (planet) {
       case "Earth":
-        marvelScore++;
-        ringsScore++;
+        marvelScore = marvelScore + 10;
+        ringsScore = ringsScore +10;
         break;
 
       case "Asgard":
-        marvelScore++;
+        marvelScore= marvelScore +10;
         break;
 
       case "Betelgeuse":
 
       case "Vogsphere":
-        hitchhikersScore++;
+        hitchhikersScore = hitchhikersScore +10;
         break;
       case "Kashyyyk":
 
       case "Endor":
-        starWarsScore++;
+        starWarsScore = starWarsScore + 10;
         break;
 
       default:
@@ -81,34 +127,57 @@ public class Main {
     if (traits != null && !traits.isEmpty()) {
     if (traits.contains("HAIRY")) {
       starWarsScore++;
+      marvelScore--;
+      hitchhikersScore--;
+      ringsScore--;
     }
     if (traits.contains("TALL")) {
       marvelScore++;
       starWarsScore++;
+      hitchhikersScore--;
+      ringsScore--;
     }
     if (traits.contains("SHORT")) {
       starWarsScore++;
+      marvelScore--;
+      hitchhikersScore--;
 
     }
     if (traits.contains("BLONDE")) {
       marvelScore++;
       ringsScore++;
+      starWarsScore--;
+      hitchhikersScore--;
     }
     if (traits.contains("EXTRA_ARMS")) {
       hitchhikersScore++;
+      starWarsScore--;
+      marvelScore--;
+      ringsScore--;
     }
     if (traits.contains("EXTRA_HEAD")) {
       hitchhikersScore++;
+      starWarsScore--;
+      marvelScore--;
+      ringsScore--;
     }
     if (traits.contains("GREEN")) {
       hitchhikersScore++;
+      starWarsScore--;
+      marvelScore--;
+      ringsScore--;
     }
     if (traits.contains("BULKY")) {
       hitchhikersScore++;
       ringsScore++;
+      starWarsScore--;
+      marvelScore--;
     }
     if (traits.contains("POINTY_EARS")) {
       ringsScore++;
+      starWarsScore--;
+      marvelScore--;
+      hitchhikersScore--;
     }}
 
 
@@ -119,23 +188,19 @@ public class Main {
     int maxScore = Math.max(Math.max(starWarsScore, marvelScore), Math.max(hitchhikersScore, ringsScore));
     System.out.println("Max Score: " + maxScore);
 
+    List<String> topUniverses = new ArrayList<>();
+    if (starWarsScore == maxScore) topUniverses.add("Star Wars");
+    if (marvelScore == maxScore) topUniverses.add("Marvel");
+    if (hitchhikersScore == maxScore) topUniverses.add("Hitchhiker's");
+    if (ringsScore == maxScore) topUniverses.add("Lord of the Rings");
 
-
-    if (maxScore == 0) {
-      System.out.println("Could not classify creature with ID: " + creature.getId());
-      return;
+    // If a tie, resolve with priority
+    if (topUniverses.size() > 1) {
+      applyTieBreaker(creature, topUniverses, starWars, marvel, hitchhikers, rings);
+    } else {
+      assignToUniverse(creature, topUniverses.get(0), starWars, marvel, hitchhikers, rings);
     }
 
-
-    if (maxScore == starWarsScore) {
-      starWars.getIndividuals().add(creature);
-    } else if (maxScore == marvelScore) {
-      marvel.getIndividuals().add(creature);
-    } else if (maxScore == hitchhikersScore) {
-      hitchhikers.getIndividuals().add(creature);
-    } else if (maxScore == ringsScore) {
-      rings.getIndividuals().add(creature);
-    }
   }
 
   public static void main(String[] args) throws IOException {
